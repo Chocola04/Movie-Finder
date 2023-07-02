79f1a704e65b1a2d9168e8eb50a8933421229734
Chocola04 <127106445+Chocola04@users.noreply.github.com>
2023-07-02 12:37:30 +0530

    added redux

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,29 +1,54 @@
 import React from 'react';
 import "./lib/font-awesome/css/card.css"
 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"></link>
+
 export const Card = ({ movie }) => {
-  return (
-    <div className="card-container">
-    <div className="card-poster" >
-    {movie.poster_path ? (
-        <img
-        //src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-         alt={`${movie.title} Poster`} 
-         /> 
-         ) : (
-        <div className = "filler-poster" >
+  // return (
+  //   <div class="card" style="width: 18rem;">
+  //   <div class="card-img-top">
+  //   {movie.poster_path ? (
+  //       <img
+  //        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+  //         alt={`${movie.title} Poster`} 
+  //        /> 
+  //        ) : (
+  //        <div className = "filler-poster" >
             
-        </div>
-    )}
-    </div>
+  //       </div>
+  //   </div> )}
+  // <div class="card-body">
+  //   <h2 class="card-title">{movie.title}</h2>
+  //   <p class="card-text">{movie.release_date?.substring(0,4)}</p>
+  //   <button type="button" class="btn btn-secondary">Add to Watchlist</button>
+  //   <button type="button" class="btn btn-secondary">Add to Watchlist</button>
+  // </div>
+  // </div>
+  // )
+
+  return (
+    <div className="card" style={{ width: '18rem' }}>
+      <div className="card-img-top">
+        {movie.poster_path ? (
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt={`${movie.title} Poster`}
+          />
+        ) : (
+          <div className="filler-poster"></div>
+        )}
+      </div>
       <div className="card-body">
         <h2 className="card-title">{movie.title}</h2>
-        <p className="card-date">{movie.release_date?.substring(0,4)}</p>
-        <div className="card-buttons">
-          <button className="card-button" >Add to Wishlist</button>
-          <button className="card-button">Watched</button>
-        </div>
-      </div> 
+        <p className="card-text">{movie.release_date?.substring(0, 4)}</p>
+        <button type="button" className="btn btn-secondary">
+          Add to Watchlist
+        </button>
+        <button type="button" className="btn btn-secondary">
+          Add to Watchlist
+        </button>
+      </div>
     </div>
   );
-};
+
+}
